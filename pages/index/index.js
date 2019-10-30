55741c8a329cbd342b3a4e412a1bbdd473b73d4a
chenhui163 <chenhui_github@163.com>
2019-10-30 15:18:41 +0800

    完成首页静态页面布局

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,7 +1,26 @@
 import request from "../../utils/request.js";
 
 Page({
-  onLoad(){
-      
-  }
+
+    data:{
+        // 模拟轮播图数据
+        slider:[
+            { url: "#", path: "" },
+            { url: "#", path: "" },
+            { url: "#", path: "" }
+        ],
+
+        // 模拟导航栏数据
+        nav:[
+            { url: "#", path: "" },
+            { url: "#", path: "" },
+            { url: "#", path: "" },
+            { url: "#", path: "" }
+        ]
+
+    },
+
+    onLoad(){
+        
+    }
 })
