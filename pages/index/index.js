06ca267350dbe4115a7e94faffb423baa6e893c9
chenhui163 <chenhui_github@163.com>
2019-10-30 16:37:24 +0800

    首页数据渲染完成

--- pages/index/index.js
+++ pages/index/index.js
@@ -3,24 +3,47 @@ import request from "../../utils/request.js";
 Page({
 
     data:{
-        // 模拟轮播图数据
-        slider:[
-            { url: "#", path: "" },
-            { url: "#", path: "" },
-            { url: "#", path: "" }
-        ],
+        // 轮播图数据
+        slider:[],
 
-        // 模拟导航栏数据
-        nav:[
-            { url: "#", path: "" },
-            { url: "#", path: "" },
-            { url: "#", path: "" },
-            { url: "#", path: "" }
-        ]
+        // 导航栏数据
+        nav:[],
 
+        // 楼层数据
+        floors:[]
     },
 
     onLoad(){
-        
+
+        // 请求轮播图数据
+        request({
+            url:"https://api.zbztb.cn/api/public/v1/home/swiperdata"
+        }).then(res=>{
+            const {message} = res.data;
+            this.setData({
+                slider: message
+            })
+        })
+
+        // 请求导航栏数据
+        request({
+            url: "https://api.zbztb.cn/api/public/v1/home/catitems"
+        }).then(res=>{
+            const { message } = res.data;
+            this.setData({
+                nav: message
+            })
+        })
+
+        // 请求楼层数据
+        request({
+            url: "https://api.zbztb.cn/api/public/v1/home/floordata"
+        }).then(res => {
+            const { message } = res.data;
+            this.setData({
+                floors: message
+            })
+        })
+
     }
 })
