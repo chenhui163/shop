d0b9d957d92fc6e54011c316a0694bd461aa97cc
chenhui163 <chenhui_github@163.com>
2019-10-31 20:59:25 +0800

    从商品详情页获取商品id，请求商品详情数据，渲染页面

--- pages/goods_detail/index.js
+++ pages/goods_detail/index.js
@@ -1,18 +1,35 @@
 // pages/goods_detail/index.js
+import request from "../../utils/request.js";
+
 Page({
 
     /**
      * 页面的初始数据
      */
     data: {
-
+        // 商品详情数据
+        info: {}
     },
 
     /**
      * 生命周期函数--监听页面加载
      */
     onLoad: function (options) {
-        
+        // 获取商品id
+        const goods_id = +options.goods_id;
+
+        // 请求商品详情数据
+        request({
+            url: "/api/public/v1/goods/detail",
+            data:{
+                goods_id
+            }
+        }).then(res=>{
+            const { message} = res.data;
+            this.setData({
+                info: message
+            })
+        })
     }
 
 })
