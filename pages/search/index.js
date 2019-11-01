d8c5bded98ff3a7280589f1af8b2d0551cc81455
chenhui163 <chenhui_github@163.com>
2019-11-01 15:58:55 +0800

    输入框点击确定（按回车键）时将关键字追加进历史记录，并跳转到商品详情页

--- pages/search/index.js
+++ pages/search/index.js
@@ -42,6 +42,34 @@ Page({
             value,
             isShowBtn: flag
         })
+    },
+
+    // 点击完成按钮（或敲回车键）时触发
+    bindConfirm(e){
+        // 获取输入框的值
+        const value = e.detail.value.trim();
+        // 如果为空，不执行以下代码
+        if(!value) return;
+        
+        // 获取本地的历史记录，如果有则获取，没有则默认空数组
+        const history = wx.getStorageSync('search') || [];
+
+        // 去重方法1：遍历数组，判断关键字是否重复，若重复就去重
+        // const arr = history.filter(v=>{
+        //     return v !== value;
+        // })
+        // arr.unshift(value);
+
+        // 去重方法2，使用new Set()方法去重
+        const arr = [value, ...new Set(history)];
+
+        // 将历史记录数组存到本地
+        wx.setStorageSync('search', arr);
+
+        // 跳到商品列表页
+        wx.navigateTo({
+            url: "/pages/goods_list/index?query=" + value
+        })
     }
 
 })
