e88c157ac63b886abdec92ab273c319b52feec39
chenhui163 <chenhui_github@163.com>
2019-11-01 15:20:30 +0800

    将input输入框的值与data中的value动态绑定，input输入框有值时则显示按钮，否则不显示

--- pages/search/index.js
+++ pages/search/index.js
@@ -5,6 +5,11 @@ Page({
      * 页面的初始数据
      */
     data: {
+        // 是否显示按钮，默认false不显示
+        isShowBtn: false,
+
+        // 输入框的值
+        value: ""
 
     },
 
@@ -20,6 +25,23 @@ Page({
      */
     onShow: function () {
 
+    },
+
+    // input输入时触发
+    bindInput(e){
+        // 获取输入框的值
+        const value = e.detail.value.trim();
+
+        // 设置开关决定是否修改isShowBtn
+        let flag;
+        // input输入框有值，则显示按钮，否则不显示
+        flag = value ? true : false;
+
+        // 修改data中的isShowBtn和value
+        this.setData({
+            value,
+            isShowBtn: flag
+        })
     }
-    
+
 })
