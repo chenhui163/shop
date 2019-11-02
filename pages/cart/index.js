d0db47a3764d74be1f0982bc8e27b72e4334367d
chenhui163 <chenhui_github@163.com>
2019-11-02 10:20:01 +0800

    判断本地是否保存有收货人信息，有则显示数据，无则显示新增提示

--- pages/cart/index.js
+++ pages/cart/index.js
@@ -5,21 +5,28 @@ Page({
      * 页面的初始数据
      */
     data: {
-
+        // 收货人信息
+        consignee: {},
+        // 是否显示添加地址，默认是true显示新增
+        isShowAddAddress: true
     },
 
     /**
      * 生命周期函数--监听页面加载
      */
     onLoad: function (options) {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面初次渲染完成
-     */
-    onReady: function () {
-
+        // 从本地获取地址信息
+        let consignee = wx.getStorageSync('consignee') || {};
+        // 判断本地的收货人信息对象consignee是否为空
+        let result = (JSON.stringify(consignee) === "{}");
+        // 如果不为空
+        if(!result){
+            // 将信息保存到data中
+            this.setData({
+                consignee,
+                isShowAddAddress: false
+            })
+        }
     },
 
     /**
@@ -29,38 +36,24 @@ Page({
 
     },
 
-    /**
-     * 生命周期函数--监听页面隐藏
-     */
-    onHide: function () {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面卸载
-     */
-    onUnload: function () {
-
-    },
-
-    /**
-     * 页面相关事件处理函数--监听用户下拉动作
-     */
-    onPullDownRefresh: function () {
-
-    },
-
-    /**
-     * 页面上拉触底事件的处理函数
-     */
-    onReachBottom: function () {
-
-    },
-
-    /**
-     * 用户点击右上角分享
-     */
-    onShareAppMessage: function () {
-
+    // 获取收货人地址
+    chooseAddress(){
+        // 调用小程序原生API接口请求授权获取收货地址
+        wx.chooseAddress({
+            success:(res) => {
+                // 将地址拼接完整
+                res.completeAddress = res.provinceName + res.cityName + res.countyName + res.detailInfo;
+                // 将信息保存到data中
+                this.setData({
+                    consignee: res,
+                    isShowAddAddress: false
+                })
+                // 从本地获取地址信息
+                let consignee = wx.getStorageSync('consignee') || {};
+                // 将信息保存到本地
+                wx.setStorageSync('consignee', this.data.consignee);
+            }
+        })
     }
+
 })
