834e9a3fba64e73f5f962f3fbc2097f2eb0e5795
chenhui163 <chenhui_github@163.com>
2019-11-05 15:56:53 +0800

    准备好请求token的参数，然后请求token并存到本地

--- pages/auth/index.js
+++ pages/auth/index.js
@@ -3,6 +3,36 @@ import request from "../../utils/request.js";
 
 Page({
 
-    
+    // 获取用户信息
+    onGotUserInfo(e){
+        // 准备请求token需要的参数
+        const { encryptedData, rawData, iv, signature } = e.detail;
+
+        // 调用接口获取登录凭证（code）
+        wx.login({
+            success(res) {
+                // 准备请求token需要的参数
+                const code = res.code;
+
+                // 请求token
+                request({
+                    url: "/api/public/v1/users/wxlogin",
+                    method: "POST",
+                    data: {
+                        encryptedData,
+                        rawData,
+                        iv,
+                        signature,
+                        code
+                    }
+                }).then(res=>{
+                    // 获取token
+                    const { token } = res.data.message;
+                    // 保存到本地
+                    wx.setStorageSync("token", token);
+                })
+            }
+        })
+    }
 
 })
