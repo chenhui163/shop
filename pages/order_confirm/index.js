a7d691145ce26a54df98060a4116f3373ef18a76
chenhui163 <chenhui_github@163.com>
2019-11-05 18:16:09 +0800

    提交订单页数据渲染完毕

--- pages/order_confirm/index.js
+++ pages/order_confirm/index.js
@@ -5,6 +5,10 @@ Page({
      * 页面的初始数据
      */
     data: {
+        // 已选择的商品
+        select: [],
+        // 实付款
+        totalPrice: 0
 
     },
 
@@ -13,6 +17,23 @@ Page({
      */
     onShow: function () {
 
+        // 从本地获取购物车数据
+        const goods = wx.getStorageSync("cart");
+
+        // 计算商品总价格
+        let totalPrice = 0;
+
+        // 选择状态为true的商品数据
+        let select = goods.filter(v=>{
+            totalPrice = (v.goods_price * v.goods_number) + totalPrice;
+            return v.isChoose === true;
+        })
+        
+        // 将已选择的商品添加到data中
+        this.setData({
+            select,
+            totalPrice: Number(totalPrice).toFixed(2)
+        })
 
     }
 })
